7b9ba6054ce8e69a552b31688b9e6b06c92243a7
Cauan Victor <cauanvictor0325@gmail.com>
2023-09-06 16:19:38 -0300

    API Node com Postgres

---
+++ create-table.js
@@ -0,0 +1,16 @@
+import { sql } from "../node-do-zero/db.js";
+
+// sql`DROP TABLE IF EXISTS videos;`.then(() => {
+//   console.log('Tabela apagada com sucesso!')
+// })
+
+sql`
+CREATE TABLE videos (
+  id TEXT PRIMARY KEY,
+  title TEXT,
+  description TEXT,
+  duration INTEGER
+);
+`.then(() => {
+  console.log('Tabela criada com sucesso!')
+})
